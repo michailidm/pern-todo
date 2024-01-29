6c36c28768c8ad0f3ee6d6b7834c89a2512d7574
michailidm <michailidm@csd.auth.gr>
2024-01-29 21:28:22 +0200

    Implement GET for one todo & all todos

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "",
+    password: "8et7#PE8Jg2!",
     host: "localhost",
     port: 5432,
     database: "perntodo"
